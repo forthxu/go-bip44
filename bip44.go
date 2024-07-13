a7273cc519806d224561855fa090dade2c12d1de
ForthXu <forthxu@californiacolleges.edu>
2024-07-13 16:57:24 +0800

    Update bip44.go

--- bip44.go
+++ bip44.go
@@ -5,8 +5,8 @@
 package bip44
 
 import (
-	"github.com/FactomProject/go-bip32"
-	"github.com/FactomProject/go-bip39"
+	"github.com/tyler-smith/go-bip32"
+	"github.com/tyler-smith/go-bip39"
 )
 
 const Purpose uint32 = 0x8000002C
